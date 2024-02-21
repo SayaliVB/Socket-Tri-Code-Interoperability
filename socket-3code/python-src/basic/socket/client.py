2be869ef78673dea827cc3423bb50bb12e965623
SayaliVB <svbayaskar@gmail.com>
2024-02-20 23:33:21 -0800

    optimised code using linter and analyzers

--- socket-3code/python-src/basic/socket/client.py
+++ socket-3code/python-src/basic/socket/client.py
@@ -1,10 +1,30 @@
+'''
+linter warning: Missing module docstring
+
+This is a client code which connects to the server
+'''
+
+
 import socket
+from time import time #standard import should be placed before builder
+import os
+import sys
+
+#analyser error: unable to import 'builder'
+#add path variable
+sys.path.append(os.path.join(os.getcwd().rsplit('/',1)[0], 'payload'))
+
 from builder import BasicBuilder
 
-from time import time
 
 
-class BasicClient(object):
+#linter warning: Class inherits from object, can be removed in python 3
+class BasicClient():
+    '''
+    linter warning: Missing class docstring
+    Client class to connect, disconnet and send message to server
+    '''
+
     def __init__(self, name, ipaddr="127.0.0.1", port=2000):
         self._clt = None
         self.name = name
@@ -15,7 +35,8 @@ class BasicClient(object):
 
         if self.ipaddr is None:
             raise ValueError("IP address is missing or empty")
-        elif self.port is None:
+        #pylint warning : unnecessary elif after raise
+        if self.port is None:
             raise ValueError("port number is missing")
 
         self.connect()
@@ -24,11 +45,19 @@ class BasicClient(object):
         self.stop()
 
     def stop(self):
+        '''
+        linter warning: Missing method docstring
+        Call the function to close connection with server.
+        '''
         if self._clt is not None:
             self._clt.close()
         self._clt = None
 
     def connect(self):
+        '''
+        linter warning: Missing method docstring
+        Call the function to connect with the server.
+        '''
         if self._clt is not None:
             return
 
@@ -38,33 +67,49 @@ class BasicClient(object):
         #self._clt.setblocking(False)
 
     def join(self, group):
+        '''
+        linter warning: Missing method docstring
+        Call the function to join/change to public/private group.
+        '''
         self.group = group
 
-    def sendMsg(self, text):
+    #pylint warning: method name does not conform to snake_case naming style
+    def send_msg(self, text):
+        '''
+        linter warning: Missing method docstring
+        Call the function to send message to the server.
+        '''
         if self._clt is None:
             raise RuntimeError("No connection to server exists")
 
         print(f"sending to group {self.group} from {self.name}: {text}")
         bldr = BasicBuilder()
-        m = bldr.encode(self.name,self.group,text)
-        self._clt.send(bytes(m, "utf-8"))
+        #linter warning redefining name from outer scope
+        msg = bldr.encode(self.name,self.group,text)
+        self._clt.send(bytes(msg, "utf-8"))
 
     def groups(self):
-        # return list of groups
-        pass
+        '''
+        linter warning: Missing method docstring
+        todo
+        '''
+        #unnecessary pass statement
 
-    def getMsgs(self):
-        # get the latest messages from a group
-        pass
-
+    def get_msgs(self):
+        '''
+        linter warning: Missing method docstring
+        todo 
+        '''
+        #unnecessary pass statement
 
 if __name__ == '__main__':
     #clt = BasicClient("frida_kahlo","192.168.1.201",2000)
     clt = BasicClient("frida_kahlo","127.0.0.1",2000)
     while True:
         m = input("enter message: ")
-        if m == '' or m == 'exit':
+        #linter warning: merge comparisons
+        #unnecessary else after break
+        if m in ('', 'exit'):
             break
-        else:
-            for i in range (2):
-                clt.sendMsg(m + " at " + str(time()))
+        for i in range (2):
+            clt.send_msg(m + " at " + str(time()))
--- socket-3code/python-src/basic/socket/server.py
+++ socket-3code/python-src/basic/socket/server.py
@@ -1,53 +1,83 @@
+'''
+linter warning: Missing module docstring
+
+This is a server code which listens to the client code
+'''
+
 import socket
 import threading
+from time import time
+import sys
+import os
+
+#analyser error: unable to import 'builder'
+#add path variable
+sys.path.append(os.path.join(os.getcwd().rsplit('/',1)[0], 'payload'))
 
 from builder import BasicBuilder
-from time import time
 
 
-class BasicServer(object):
-   def __init__(self, ipaddr, port=2000):
-      self.ipaddr = ipaddr
-      self.port = port
-      self._svr = None
-      self.good = True
+#linter warning: Class inherits from object, can be removed in python 3
+class BasicServer():
+    '''
+    linter warning: Missing class docstring
+    Server class to listen to client
+    '''
+    def __init__(self, ipaddr, port=2000):
+        self.ipaddr = ipaddr
+        self.port = port
+        self._svr = None
+        self.good = True
 
-      if self.ipaddr is None:
-           raise ValueError("IP address is missing or empty")
-      elif self.port is None:
-           raise ValueError("port number is missing")
-      elif self.port <=1024:
-           raise ValueError(f"port number ({port}) must be above 1024")
+        #pylint warning : unnecessary elif after raise
+        if self.ipaddr is None:
+            raise ValueError("IP address is missing or empty")
+        if self.port is None:
+            raise ValueError("port number is missing")
+        if self.port <=1024:
+            raise ValueError(f"port number ({port}) must be above 1024")
 
-   def __del__(self):
-      # close socket
-      self.stop()
+    def __del__(self):
+        '''close socket'''
+        self.stop()
 
-   def stop(self):
-      self.good = False
-      if self._svr is not None:
-         self._svr.close()
-         self._svr = None
- 
-   def run(self):
-      addr = (self.ipaddr,self.port)
-      if socket.has_dualstack_ipv6():
-         self._svr = socket.create_server(addr, family=socket.AF_INET6, dualstack_ipv6=True)
-      else:
-         self._svr = socket.create_server(addr)
-      self._svr.listen(10)
+    def stop(self):
+        '''
+        linter warning: Missing method docstring
+        closes the connection from client if client is lost
+        '''
+        self.good = False
+        if self._svr is not None:
+            self._svr.close()
+            self._svr = None
 
-      print(f"server ({self.ipaddr}) is listening on {self.port}")
+    def run(self):
+        '''
+        linter warning: Missing method docstring
+        server listens for client connection
+        '''
+        addr = (self.ipaddr,self.port)
+        if socket.has_dualstack_ipv6():
+            self._svr = socket.create_server(addr, family=socket.AF_INET6, dualstack_ipv6=True)
+        else:
+            self._svr = socket.create_server(addr)
+        self._svr.listen(10)
 
-      while self.good:
-        cltconn, caddr = self._svr.accept()
-        print(f"Connection from {caddr[0]}")
-        csession = SessionHandler(cltconn,caddr)
-        csession.start()
+        print(f"server ({self.ipaddr}) is listening on {self.port}")
+
+        while self.good:
+            cltconn, caddr = self._svr.accept()
+            print(f"Connection from {caddr[0]}")
+            csession = SessionHandler(cltconn,caddr)
+            csession.start()
 
 # ----------------------------------------------
 
 class SessionHandler(threading.Thread):
+    '''
+    linter warning: Missing class docstring
+    Session class to start a session for a client when client connectio is established
+    '''
     def __init__(self,client_connection, client_addr):
         threading.Thread.__init__(self)
         self.daemon = False
@@ -59,19 +89,29 @@ class SessionHandler(threading.Thread):
         self.close()
 
     def close(self):
+        '''
+        linter warning: Missing method docstring
+        closes the connection from client if client is lost
+        '''
         if self._cltconn is None:
             return
-        else:
-            self._cltconn.close()
-            self._cltconn = None
-            self.good = False
+        #unnecessary else after break
+        self._cltconn.close()
+        self._cltconn = None
+        self.good = False
 
     def process(self,raw):
+        '''
+        linter warning: Missing method docstring
+        recives and processes data from and client
+        '''
         try:
             bldr = BasicBuilder()
             name,group,text = bldr.decode(raw)
             print(f"from {name}, to group: {group}, text: {text} , at {str(time())}")
-        except Exception as e:
+        #unused variable e
+        #Catching too general exception
+        except Exception:
             pass
 
     def run(self):
@@ -82,6 +122,7 @@ class SessionHandler(threading.Thread):
                     self.good = False
                 else:
                     self.process(buf.decode("utf-8"))
+            #Catching too general exception
             except Exception as e:
                 print(e)
                 self.good = False
